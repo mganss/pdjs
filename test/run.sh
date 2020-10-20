a302772743ef50c9a745bc86e9ac5d1ecdd2d3dc
Michael Ganss <michael.ganss@updatestar.com>
2020-10-20 16:04:58 +0200

    Add PD binaries for testing Add WSL CMake settings

--- test/run.sh
+++ test/run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-$PD -nogui -stderr -batch -open test.${TRIPLET}.pd -send "test bang" \
+$PD -noprefs -nrt -nogui -stderr -batch -open test.${TRIPLET}.pd -send "test bang" \
     > result.${TRIPLET}.out.txt 2> result.${TRIPLET}.err.txt
 
 rm -f result.${TRIPLET}.txt
 
 cat result.${TRIPLET}.out.txt result.${TRIPLET}.err.txt \
-    | egrep -v "priority [0-9]+ scheduling failed" \
     > result.${TRIPLET}.txt
--- test/test.sh
+++ test/test.sh
@@ -2,16 +2,13 @@
 
 if [ "$OS" = "Windows_NT" ]; then
     export TRIPLET="x64-windows-static"
-    pacman -S curl unzip -q --noconfirm
-    curl -s http://msp.ucsd.edu/software.html | egrep -o 'http[^"]*\.msw\.zip' | head -1 | xargs -n 1 curl -s -o /tmp/pd.zip
-    unzip -o -qq /tmp/pd.zip
-    export PD="`ls | grep pd-`/bin/pd.com"
 elif [ "$OSTYPE" = "linux-gnu" ]; then
     if [ "$HOSTTYPE" = "x86_64" ]; then
         export TRIPLET="x64-linux"
     fi
-    export PD="`which pd`"
 fi
+
+export PD="pd/${TRIPLET}/bin/pd"
 
 . run.sh
 
