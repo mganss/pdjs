bad6adadeb9047535ca62b42d5d229dc5d91268b
Michael Ganss <michael.ganss@updatestar.com>
2020-11-11 17:41:50 +0100

    Remove vcpkg Add arm64-linux

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 
 PD_EXT=""
 if [ "$OS" = "Windows_NT" ]; then
-    export TRIPLET="x64-windows-static"
+    export TRIPLET="x64-windows"
     PD_EXT=".com"
     pacman -q --needed --noconfirm -S diffutils > /dev/null 2>&1
 elif [ "$OSTYPE" = "linux-gnu" ]; then
@@ -28,12 +28,15 @@ for TESTFILE in test-*/test-*.pd; do
 
     . ../run.sh $TEST
 
-    V8_VERSION=`find ../../vcpkg-export/ -type f -name v8_monolith.pc -exec egrep -o 'Version: [0-9.]+' {} \; | egrep -o '[0-9.]+' | head -n1`
-    sed "s/pdjs version.*/pdjs version ${VERSION} (v8 version ${V8_VERSION})/" < ./result.txt > ./expected.txt
+    cp ./result.txt ./expected.txt
+    cp ./result.${TRIPLET}.txt ./actual.txt
+
+    sed -i -r "s/^pdjs version.*/pdjs version/" ./expected.txt
+    sed -i -r "s/^pdjs version.*/pdjs version/" ./actual.txt
 
     EXCEPTION_REGEX="s/^.+\.js:[0-9]+:(.+)/\1/"
     sed -i -r "${EXCEPTION_REGEX}" ./expected.txt
-    sed -r "${EXCEPTION_REGEX}" < ./result.${TRIPLET}.txt > ./actual.txt
+    sed -i -r "${EXCEPTION_REGEX}" ./actual.txt
 
     ERROR_REGEX="s/^(error: Error.*) '.*\.js':/\1/"
     sed -i -r "${ERROR_REGEX}" ./expected.txt
